b8f3e3e68a2c8c3ae332f7610fd828f79b2f1746
bbha99 <branha99@gmail.com>
2023-03-09 23:14:57 -0800

    implement admin display sale status page

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,15 @@
 class Sale < ApplicationRecord
+
+  # Business logic here instead of in the views
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end

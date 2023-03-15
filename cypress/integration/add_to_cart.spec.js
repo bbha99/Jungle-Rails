08dbd6cfb5a6ef5e90ac833059af6797287c869d
bbha99 <branha99@gmail.com>
2023-03-14 17:31:33 -0700

    implement add to cart spec feature

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('visiting the homepage', () => {
+
+  it("displays the homepage content", () => {
+    cy.visit('/').contains("Welcome to")
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("should click the add to cart button of the first product", () => {
+    cy.get('article [action="/cart/add_item?product_id=1"] .btn').click()
+    cy.get(".nav-link").contains("My Cart (1)")
+  });
+
+})
